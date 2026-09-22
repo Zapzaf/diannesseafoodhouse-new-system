6fa735ebd2cae44ca378531cb60f2dd4f56b7f4c
Zapzaf <realesgian@gmail.com>
2026-09-22 20:41:42 +0800

    Test

--- resources/views/check-register/index.blade.php
+++ resources/views/check-register/index.blade.php
@@ -7,7 +7,7 @@
     <div class="container-xl px-4">
         @include('layouts.alerts')
 
-        <div class="card mb-4">
+        <div class="card mb-4" data-static-pagination="1">
             <div class="card-header d-flex align-items-center justify-content-between gap-2">
                 <div><i class="me-1" data-lucide="check-square"></i> Issued Checks</div>
             </div>
--- resources/views/check-vouchers/index.blade.php
+++ resources/views/check-vouchers/index.blade.php
@@ -52,7 +52,7 @@
             </div>
         </div>
 
-        <div class="card mb-4">
+        <div class="card mb-4" data-static-pagination="1">
             <div class="card-header d-flex align-items-center justify-content-between gap-2">
                 <div><i class="me-1" data-lucide="banknote"></i> Disbursements</div>
             </div>
